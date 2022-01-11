3be04fa9a9fa12b09d172a9f91a75116078ac1fa
Leah_Grace <leahgdg@hotmail.com>
2022-01-11 10:42:04 -0500

    "passed"

---
+++ src/com/LeahGrace/SortArray.java
@@ -0,0 +1,11 @@
+package com.LeahGrace;
+
+import java.util.Arrays;
+
+public class SortArray {
+
+        public static int[] sortNumsAscending(int[] nums) {
+            Arrays.sort(nums);
+            return nums;
+        }
+    }
